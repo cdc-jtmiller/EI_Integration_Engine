78b0fd153333906355e1aeae2aeb7eb0821232ac
Jeremy Miller <miller.jeremyt@gmail.com>
2024-05-20 17:12:48 -0400

    Initial commit: POC for Epi Info Integration Engine

---
+++ ParseHL7/frmParseHL7.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace ParseHL7
+{
+    partial class frmParseHL7
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tbHL7Path = new TextBox();
+            lblHL7Path = new Label();
+            btnBrowseHL7 = new Button();
+            openFileDialog = new OpenFileDialog();
+            tabControl1 = new TabControl();
+            tabPage3 = new TabPage();
+            rtbRaw = new RichTextBox();
+            tabPage1 = new TabPage();
+            rtbXML = new RichTextBox();
+            tabPage5 = new TabPage();
+            rtbXml2Json = new RichTextBox();
+            tabPage4 = new TabPage();
+            rtbFHIR = new RichTextBox();
+            lblEIIE = new Label();
+            tabControl1.SuspendLayout();
+            tabPage3.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage5.SuspendLayout();
+            tabPage4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tbHL7Path
+            // 
+            tbHL7Path.Location = new Point(99, 49);
+            tbHL7Path.Name = "tbHL7Path";
+            tbHL7Path.Size = new Size(860, 27);
+            tbHL7Path.TabIndex = 0;
+            // 
+            // lblHL7Path
+            // 
+            lblHL7Path.AutoSize = true;
+            lblHL7Path.Font = new Font("Lucida Sans", 11.25F, FontStyle.Regular, GraphicsUnit.Point,  0);
+            lblHL7Path.Location = new Point(12, 54);
+            lblHL7Path.Name = "lblHL7Path";
+            lblHL7Path.Size = new Size(77, 17);
+            lblHL7Path.TabIndex = 1;
+            lblHL7Path.Text = "HL7 Path:";
+            // 
+            // btnBrowseHL7
+            // 
+            btnBrowseHL7.Font = new Font("Lucida Sans", 9.75F, FontStyle.Regular, GraphicsUnit.Point,  0);
+            btnBrowseHL7.Location = new Point(978, 51);
+            btnBrowseHL7.Name = "btnBrowseHL7";
+            btnBrowseHL7.Size = new Size(75, 23);
+            btnBrowseHL7.TabIndex = 2;
+            btnBrowseHL7.Text = "Browse";
+            btnBrowseHL7.UseVisualStyleBackColor = true;
+            btnBrowseHL7.Click += btnBrowseHL7_Click;
+            // 
+            // openFileDialog
+            // 
+            openFileDialog.FileName = "openFileDialog";
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage5);
+            tabControl1.Controls.Add(tabPage4);
+            tabControl1.Font = new Font("Lucida Sans", 11.25F, FontStyle.Regular, GraphicsUnit.Point,  0);
+            tabControl1.Location = new Point(12, 119);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1041, 440);
+            tabControl1.TabIndex = 4;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(rtbRaw);
+            tabPage3.Location = new Point(4, 26);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Size = new Size(1033, 410);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Raw";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // rtbRaw
+            // 
+            rtbRaw.Font = new Font("Lucida Console", 11.25F, FontStyle.Regular, GraphicsUnit.Point,  0);
+            rtbRaw.Location = new Point(3, 3);
+            rtbRaw.Name = "rtbRaw";
+            rtbRaw.Size = new Size(1027, 404);
+            rtbRaw.TabIndex = 0;
+            rtbRaw.Text = "";
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(rtbXML);
+            tabPage1.Location = new Point(4, 26);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1033, 410);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "XML";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // rtbXML
+            // 
+            rtbXML.Font = new Font("Lucida Console", 11.25F, FontStyle.Regular, GraphicsUnit.Point,  0);
+            rtbXML.Location = new Point(6, 6);
+            rtbXML.Name = "rtbXML";
+            rtbXML.Size = new Size(1021, 398);
+            rtbXML.TabIndex = 5;
+            rtbXML.Text = "";
+            // 
+            // tabPage5
+            // 
+            tabPage5.Controls.Add(rtbXml2Json);
+            tabPage5.Location = new Point(4, 26);
+            tabPage5.Name = "tabPage5";
+            tabPage5.Size = new Size(1033, 410);
+            tabPage5.TabIndex = 4;
+            tabPage5.Text = "JSON";
+            tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // rtbXml2Json
+            // 
+            rtbXml2Json.Location = new Point(3, 3);
+            rtbXml2Json.Name = "rtbXml2Json";
+            rtbXml2Json.Size = new Size(1027, 404);
+            rtbXml2Json.TabIndex = 2;
+            rtbXml2Json.Text = "";
+            // 
+            // tabPage4
+            // 
+            tabPage4.Controls.Add(rtbFHIR);
+            tabPage4.Font = new Font("Lucida Console", 11.25F, FontStyle.Regular, GraphicsUnit.Point,  0);
+            tabPage4.Location = new Point(4, 26);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Size = new Size(1033, 410);
+            tabPage4.TabIndex = 3;
+            tabPage4.Text = "FHIR";
+            tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // rtbFHIR
+            // 
+            rtbFHIR.Location = new Point(3, 3);
+            rtbFHIR.Name = "rtbFHIR";
+            rtbFHIR.Size = new Size(1027, 404);
+            rtbFHIR.TabIndex = 0;
+            rtbFHIR.Text = "";
+            // 
+            // lblEIIE
+            // 
+            lblEIIE.AutoSize = true;
+            lblEIIE.Font = new Font("Lucida Sans", 15.75F, FontStyle.Bold, GraphicsUnit.Point,  0);
+            lblEIIE.Location = new Point(11, 9);
+            lblEIIE.Name = "lblEIIE";
+            lblEIIE.Size = new Size(376, 23);
+            lblEIIE.TabIndex = 7;
+            lblEIIE.Text = "EIIE : : Epi Info Integration Engine";
+            // 
+            // frmParseHL7
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1074, 627);
+            Controls.Add(lblEIIE);
+            Controls.Add(tabControl1);
+            Controls.Add(btnBrowseHL7);
+            Controls.Add(lblHL7Path);
+            Controls.Add(tbHL7Path);
+            Name = "frmParseHL7";
+            Text = "Parse HL7";
+            tabControl1.ResumeLayout(false);
+            tabPage3.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage5.ResumeLayout(false);
+            tabPage4.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox tbHL7Path;
+        private Label lblHL7Path;
+        private Button btnBrowseHL7;
+        private OpenFileDialog openFileDialog;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private RichTextBox rtbXML;
+        private TabPage tabPage3;
+        private RichTextBox rtbRaw;
+        private TabPage tabPage4;
+        private Label lblEIIE;
+        private RichTextBox rtbFHIR;
+        private TabPage tabPage5;
+        private RichTextBox rtbXml2Json;
+    }
+}
---
+++ ParseHL7/frmParseHL7.cs
@@ -0,0 +1,464 @@
+using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.Xml.Linq;
+using NHapi.Base;
+using NHapi.Base.Parser;
+using NHapi.Base.Model;
+using NHapi.Base.Util;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Formatting = Newtonsoft.Json.Formatting;
+using Newtonsoft.Json.Serialization;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
+using System.Reflection.Metadata;
+
+
+namespace ParseHL7
+{
+    public partial class frmParseHL7 : Form
+    {
+        // Firely public server
+        private const string _fhirServer = "http://vonk.fire.ly";
+
+        // List to collect and manage error messages
+        private List<string> errorCollection = new List<string>();
+        // Current segment being processed
+        private string currentSegment = string.Empty;
+        // Current field number being processed
+        private int fieldNumber = 0;
+        // List of CSegment to manage segment data
+        private List<CSegment> segments = new List<CSegment>();
+
+
+        public frmParseHL7()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowseHL7_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                // Set the initial directory (optional)
+                string msgPath = Path.GetFullPath(Path.Combine(Application.StartupPath, @"..\..\..\")) + @"messages\";
+                //openFileDialog.InitialDirectory = @"C:\Temp\HL7\Messages\GenV2";
+                openFileDialog.InitialDirectory = msgPath;
+                openFileDialog.RestoreDirectory = true;
+
+                // Set the title of the dialog (optional)
+                openFileDialog.Title = "Open HL7 File";
+
+                // Set the filter to restrict file types (optional)
+                openFileDialog.Filter = "Text files (*.hl7)|*.hl7|All files (*.*)|*.*";
+
+                // Open the dialog and check if the user selected a file
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Get the selected file path
+                    string filePath = openFileDialog.FileName;
+
+                    // Display the file path in the text box
+                    tbHL7Path.Text = filePath;
+                    execCode();
+                }
+            }
+        }
+
+        private void execCode()
+        {
+            if (rtbRaw.Text != String.Empty)
+            {
+                rtbRaw.SelectAll();
+                rtbRaw.Clear();
+
+                rtbXML.SelectAll();
+                rtbXML.Clear();
+
+                rtbXml2Json.SelectAll();
+                rtbXml2Json.Clear();
+
+                rtbFHIR.SelectAll();
+                rtbFHIR.Clear();
+            }
+
+            string filePath = tbHL7Path.Text;
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                string hl7Content = File.ReadAllText(filePath);
+                DisplayRAW(hl7Content);
+
+                try
+                {
+                    var parser = new PipeParser();
+                    IMessage parsedMessage = parser.Parse(hl7Content.Trim());
+
+                    if (parsedMessage != null)
+                    {
+                        var root = new XElement("HL7Message");
+                        LoopOnStructures(parsedMessage, root);
+
+                        XElement xmlElement = DisplayXML(root);
+                        DisplayJSONxXML(xmlElement);
+
+                        GenerateFHIRMessage(parsedMessage);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred while processing the file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("You must enter a valid path.");
+            }
+
+        }
+
+        private void LoopOnStructures(IStructure structure, XElement parentElement)
+        {
+            if (structure is ISegment segment)
+            {
+                var segmentElement = new XElement(segment.GetStructureName());
+                parentElement.Add(segmentElement);
+
+                for (int i = 1; i <= segment.NumFields(); i++)
+                {
+                    var field = segment.GetField(i);
+                    var fieldElement = new XElement($"Field{i}");
+                    segmentElement.Add(fieldElement);
+
+                    ExtractFieldData(fieldElement, field);
+                }
+            }
+            else if (structure is IGroup group)
+            {
+                var groupElement = new XElement(group.GetStructureName());
+                parentElement.Add(groupElement);
+
+                foreach (var name in group.Names)
+                {
+                    var children = group.GetAll(name);
+                    foreach (var child in children)
+                    {
+                        LoopOnStructures(child, groupElement);
+                    }
+                }
+            }
+        }
+
+        private void LoopOnFields(IType[] types, int level = 0)
+        {
+            foreach (IType type in types)
+            {
+                //Field could be composite or primitive
+                if (type is IComposite compositeItem)
+                {
+                    LoopOnFields(compositeItem.Components.ToArray(), level + 1);
+                }
+                else
+                {
+                    //Here we can find structural issues.
+                    if (type is IPrimitive primitive)
+                    {
+                        int extraComponentCount = primitive.ExtraComponents.numComponents();
+                        if (extraComponentCount > 0)
+                        {
+                            CSegment _cseg = segments.FirstOrDefault(cseg => cseg.Name.Equals(currentSegment));
+                            if (_cseg != null)
+                            {
+                                errorCollection.Add(string.Format("{0}^{1}^{2}^102&Data type error&HL7nnnn", currentSegment, _cseg.Sequence, fieldNumber));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void LoopOnFields(IType[] types)
+        {
+            foreach (IType type in types)
+            {
+                //Field could be composite or primitive
+                IComposite compositeItem = type as IComposite;
+                if (compositeItem != null)
+                    LoopOnFields((IType[]) compositeItem.Components);
+
+                //Here we can find structural issues.
+                if (type.ExtraComponents.numComponents() > 0)
+                {
+                    CSegment _cseg = segments.Where(cseg => cseg.Name.Equals(currentSegment)).First<CSegment>();
+                    errorCollection.Add(string.Format("{0}^{1}^{2}^102&Data type error&HL7nnnn", currentSegment, _cseg.Sequence, fieldNumber));
+                }
+            }
+        }
+
+        public static string ConvertXmlToJson(XElement xmlElement)
+        {
+            try
+            {
+                string json = JsonConvert.SerializeXNode(xmlElement);
+
+                // Optional: Format the JSON string for better readability
+                JToken parsedJson = JToken.Parse(json);
+                string formattedJson = parsedJson.ToString(Formatting.Indented);
+
+                return formattedJson;
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that occur during the conversion
+                Console.WriteLine($"Error converting XML to JSON: {ex.Message}");
+                return null;
+            }
+        }
+
+        private void DisplayRAW(string messageText)
+        {
+            rtbRaw.Text = messageText;
+        }
+
+        private XElement DisplayXML(XElement root)
+        {
+            string xmlContent = root.ToString();
+            rtbXML.Text = xmlContent;
+            return root;
+        }
+
+        private void DisplayJSONxXML(XElement root)
+        {
+            string jsonContent = ConvertXmlToJson(root);
+            rtbXml2Json.Text = jsonContent;
+        }
+
+        private void ExtractFieldData(XElement fieldElement, IType[] fields)
+        {
+            foreach (IType field in fields)
+            {
+                if (field is IComposite composite)
+                {
+                    // Process composite fields recursively
+                    foreach (var component in composite.Components)
+                    {
+                        var componentElement = new XElement("Component");
+                        ExtractFieldData(componentElement, new[] { component });
+                        if (componentElement.HasElements || !string.IsNullOrEmpty(componentElement.Value))
+                        {
+                            fieldElement.Add(componentElement);
+                        }
+                    }
+                }
+                else if (field is IPrimitive primitive)
+                {
+                    // Extract the value from primitive fields
+                    string value = primitive.Value?.ToString() ?? string.Empty;
+                    fieldElement.Value = value;
+                }
+                else if (field is Varies varies)
+                {
+                    // Handle Varies type
+                    IType data = varies.Data;
+                    if (data is IPrimitive primitiveData)
+                    {
+                        string value = primitiveData.Value?.ToString() ?? string.Empty;
+                        fieldElement.Value = value;
+                    }
+                    else if (data is IComposite compositeData)
+                    {
+                        // Recursive data extraction for composite data within Varies type
+                        foreach (var component in compositeData.Components)
+                        {
+                            var componentElement = new XElement("Component");
+                            ExtractFieldData(componentElement, new[] { component });
+                            if (componentElement.HasElements || !string.IsNullOrEmpty(componentElement.Value))
+                            {
+                                fieldElement.Add(componentElement);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        fieldElement.Value = data?.ToString() ?? string.Empty;
+                    }
+                }
+                else
+                {
+                    fieldElement.Value = field?.ToString() ?? string.Empty;
+                }
+            }
+        }
+
+        private void GenerateFHIRMessage(IMessage parsedMessage)
+        {
+            try
+            {
+                // Use a list to collect JSON objects
+                var fhirJsonList = new List<JObject>();
+
+                // Traverse the HL7 message structures and build the JSON objects
+                LoopOnStructuresFHIR(parsedMessage, fhirJsonList);
+
+                // Serialize the list of JSON objects to a JSON array
+                var fhirJsonArray = JArray.FromObject(fhirJsonList);
+
+                // Format the JSON output
+                var formattedJson = fhirJsonArray.ToString(Formatting.Indented);
+
+                // Display the JSON output in the rtbFHIR rich text box
+                rtbFHIR.Text = formattedJson;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error generating FHIR message: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LoopOnStructuresFHIR(IStructure structure, List<JObject> fhirJsonList)
+        {
+            if (structure is ISegment segment)
+            {
+                var segmentName = segment.GetStructureName();
+
+                for (int i = 1; i <= segment.NumFields(); i++)
+                {
+                    var field = segment.GetField(i);
+                    foreach (var fieldValue in field)
+                    {
+                        var extractedValue = ExtractFieldValue(fieldValue);
+                        if (!string.IsNullOrEmpty(extractedValue))
+                        {
+                            // Construct the key based on the segment name and field number
+                            var key = $"{segmentName}_Field{i}";
+
+                            // Create the JSON object with the key-value pair
+                            var jsonObject = new JObject
+                    {
+                        { key, extractedValue }
+                    };
+
+                            fhirJsonList.Add(jsonObject);
+                        }
+                    }
+                }
+            }
+            else if (structure is IGroup group)
+            {
+                foreach (var name in group.Names)
+                {
+                    var children = group.GetAll(name);
+                    foreach (var child in children)
+                    {
+                        LoopOnStructuresFHIR(child, fhirJsonList);
+                    }
+                }
+            }
+        }
+
+        private string ExtractFieldValue(IType field)
+        {
+            if (field is AbstractPrimitive primitive)
+            {
+                return primitive.Value;
+            }
+            else if (field is IComposite composite)
+            {
+                var componentValues = composite.Components.Select(ExtractFieldValue).Where(val => !string.IsNullOrEmpty(val));
+                return string.Join("^", componentValues);
+            }
+            return null;
+        }
+
+        private DomainResource CreateFhirResource(string segmentName)
+        {
+            switch (segmentName)
+            {
+                case "MSH":
+                    return new MessageHeader();
+                case "EVN":
+                    return new MessageHeader();
+                case "PID":
+                    return new Patient();
+                case "PD1":
+                    return new Patient();
+                case "NK1":
+                    return new RelatedPerson();
+                case "PV1":
+                    return new Encounter();
+                case "PV2":
+                    return new Encounter();
+                case "IN1":
+                    return new Coverage();
+                case "IN2":
+                    return new Coverage();
+                case "IN3":
+                    return new Coverage();
+                case "GT1":
+                    return new RelatedPerson();
+                case "AL1":
+                    return new AllergyIntolerance();
+                case "ORC":
+                    return new ServiceRequest();
+                case "OBR":
+                    return new ServiceRequest();
+                case "OBX":
+                    return new Observation();
+                case "NTE":
+                    return new Basic
+                    {
+                        Code = new CodeableConcept
+                        {
+                            Coding = new List<Coding>
+                            {
+                                new Coding
+                                {
+                                    System = "http://hl7.org/fhir/StructureDefinition/NTE",
+                                    Code = "NTE",
+                                    Display = "Notes and Comments"
+                                }
+                            }
+                        }
+                    };
+
+                case "DG1":
+                    return new Condition();
+                case "PR1":
+                    return new Procedure();
+                case "ROL":
+                    return new PractitionerRole();
+                case "ARV":
+                    return new Consent();
+                case "DRG":
+                    return new ExplanationOfBenefit();
+                case "FT1":
+                    return new ExplanationOfBenefit();
+                case "CTI":
+                    return new ResearchStudy();
+                default:
+                    throw new ArgumentException($"Unsupported HL7 segment type: {segmentName}");
+            }
+        }
+
+        class CSegment
+        {
+            public string Name
+            {
+                get;
+                set;
+            }
+
+            public int Sequence
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
